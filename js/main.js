a2061a40d247ec1b8f12263d8b7529da15054cdd
teerawat <teerawat@wuttiwat.com>
2013-12-16 10:27:38 +0700

    Rename namedconfig.xml to confic.xml

--- js/main.js
+++ js/main.js
@@ -1,18 +1,5 @@
 var app = {
 
-    findByName: function() {
-        console.log('findByName');
-        this.store.findByName($('.search-key').val(), function(employees) {
-            var l = employees.length;
-            var e;
-            $('.employee-list').empty();
-            for (var i=0; i<l; i++) {
-                e = employees[i];
-                $('.employee-list').append('<li><a href="#employees/' + e.id + '">' + e.firstName + ' ' + e.lastName + '</a></li>');
-            }
-        });
-    },
-
     showAlert: function(message, title) {
         if (navigator.notification) {
             navigator.notification.alert(message, null, title, 'OK');
@@ -23,10 +10,9 @@ var app = {
 
     initialize: function() {
         var self = this;
-        this.store = new LocalStorageStore(function() {
-           self.showAlert('Store Initialized', 'Info');
+        this.store = new MemoryStore(function() {
+            $('body').html(new HomeView(self.store).render().el);
         });
-        $('.search-key').on('keyup', $.proxy(this.findByName, this));
     }
 
 };
